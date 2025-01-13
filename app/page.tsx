d3a8539808641a896c4edda9846031a589fc199c
MuhammadTeguh07 <muhammadteguh301@gmail.com>
2025-01-13 22:45:00 +0700

    Update css login

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 dark:bg-neutral-950">
       <div className="w-full max-w-sm">
         <Login />
       </div>
